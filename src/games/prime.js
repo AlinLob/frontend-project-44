8065b77b057c1c4a7f722d7ec36f40199a38396e
alinlob <alinalobova17@gmail.com>
2023-01-07 17:47:59 +0300

    add src/games/prime.js

---
+++ src/games/prime.js
@@ -0,0 +1,29 @@
+import playGame from '../index.js';
+
+// eslint-disable-next-line import/no-duplicates
+import { getRandom } from '../index.js';
+
+const taskGame = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+
+const getPrime = (number) => {
+  if (number <= 1) {
+    return false;
+  }
+  for (let i = 2; i < number; i += 1) {
+    if (number % i === 0) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const roundGame = () => {
+  const number = Math.abs(getRandom(1, 100));
+  const question = number;
+  const correctAnswer = getPrime(number) ? 'yes' : 'no';
+  return [question, correctAnswer];
+};
+
+export default () => {
+  playGame(taskGame, roundGame);
+};
