a1b0627b4a4638a2b2960e813f597c3a9d44b2da
alinlob <alinalobova17@gmail.com>
2023-01-12 17:30:10 +0300

    add src/games/even.js

--- src/games/even.js
+++ src/games/even.js
@@ -1,14 +1,14 @@
 /* eslint-disable import/no-duplicates */
 import playGame from '../index.js';
 
-import { getRandom } from '../index.js';
+import getRandomNum from '../getRandom.js';
 
 const taskGame = 'Answer "yes" if the number is even, otherwise answer "no".';
 
 const even = (Number) => Number % 2 === 0;
 
 const roundGame = () => {
-  const Number = getRandom(1, 1000);
+  const Number = getRandomNum(1, 1000);
   const question = `${Number}`;
   const correctAnswer = even(Number) ? 'yes' : 'no';
   return [question, correctAnswer];
