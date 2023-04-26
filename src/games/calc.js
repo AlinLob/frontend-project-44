fba95b01d860eaf7f2b1b127b29cefc432f2175a
alinlob <alinalobova17@gmail.com>
2023-04-26 13:46:36 +0300

    add src/games/calc.js

--- src/games/calc.js
+++ src/games/calc.js
@@ -1,8 +1,7 @@
-/* eslint-disable consistent-return */
 /* eslint-disable default-case */
-// eslint-disable-next-line import/no-duplicates
+/* eslint-disable consistent-return */
 import playGame from '../index.js';
-// eslint-disable-next-line import/no-duplicates
+
 import getRandomNum from '../getRandom.js';
 
 const taskGame = 'What is the result of the expression?';
